da576098c6acc393ec9da001ec732bdf170301bd
Sebastian <s.romhany@gmail.com>
2022-04-20 11:00:46 -0400

    Initial skeleton html and css + github logo icon. Empty js file.

---
+++ book-library.js
@@ -0,0 +1,11 @@
+
+
+function bookconstructor(title, author, pages, readStatus){
+    this.title = title;
+    this.author = author;
+    this.pages = pages ;
+    this.readStatus = readStatus;
+    this.info = function (){
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`
+    }
+}
