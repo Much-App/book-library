a635f8f9348031cb3a20aa5e1cba33c8ec09517d
Sebastian <s.romhany@gmail.com>
2022-04-22 12:07:25 -0400

    Skeleton html and css finalized. Initial javascript setup with array, constructor and function to add to array.

--- book-library.js
+++ book-library.js
@@ -1,6 +1,14 @@
 
+const library = [];
 
-function bookconstructor(title, author, pages, readStatus){
+
+function addBookToLibrary (){
+    const newBook = Array.from(document.querySelectorAll('#addBookForm input')).reduce((acc,input)=> ({...acc,[input.id]: input.value}),{});
+    library.push(newBook);
+    formPopup();
+}
+
+function book (title, author, pages, readStatus){
     this.title = title;
     this.author = author;
     this.pages = pages ;
@@ -9,3 +17,19 @@ function bookconstructor(title, author, pages, readStatus){
         return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`
     }
 }
+
+
+/* Toggle add book form */
+const addBookForm = document.querySelector('#addBookContainer');
+const addBookBtn = document.querySelector('#addBookBtn');
+
+addBookBtn.addEventListener('click',formPopup);
+
+function formPopup (){
+    addBookForm.classList.toggle('active');
+    document.querySelector('#addBookForm').reset();
+}
+
+
+
+
